82415ed70c3ea1482b5bd003626a3db94d93cdcf
sevenlist <sevenlist@web.de>
2014-02-21 23:31:03 +0100

    Test added: A can be persisted when detached B is merged before.

--- src/test/groovy/com/sevenlist/jpaplayground/datamodel/PersistTest.groovy
+++ src/test/groovy/com/sevenlist/jpaplayground/datamodel/PersistTest.groovy
@@ -33,6 +33,23 @@ class PersistTest extends AbstractDatabaseTestcase {
         e.message.contains 'detached entity'
     }
 
+    def "A can be persisted when detached B is merged before"() {
+        given:
+        B b = new B()
+
+        when:
+        persistEntityAndCommit(b)
+
+        B mergedB = mergeEntity(b)
+        A a = new A(b: mergedB)
+        persistEntityAndCommit(a)
+
+        A foundA = findEntity(a)
+
+        then:
+        foundA.b
+    }
+
     def "detached B can be persisted by merging A"() {
         given:
         B b = new B()
